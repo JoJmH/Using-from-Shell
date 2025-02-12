b2ac2bb6b041e3d167232f59add09686d85f4542
JoJmH <joudi.jomah@ru.nl>
2025-02-12 17:13:25 +0100

    updating the src file with the new code and file

--- python-project/src/main.py
+++ python-project/src/main.py
@@ -1,6 +1,15 @@
-# File: /python-project/python-project/src/main.py
+import new_script
+
 def main():
     print("Welcome to the Python Project!")
 
+def subtract():
+    print(f"Subtracting two numbers and the answer is:", new_script.subtract(5, 3))
+    
+def add():
+    print(f"Adding two numbers and the answer is:", new_script.add(3, 5))
+
 if __name__ == "__main__":
     main()
+    subtract()
+    add()
---
+++ python-project/src/new_script.py
@@ -0,0 +1,14 @@
+
+
+def subtract(a, b):
+    return a - b
+def add(a, b):
+    return a + b
+def greeting():
+    print("Hello, Girl!") 
+
+print(add(8, 4))
+
+if __name__ == "__main__": 
+    print(subtract(5, 3))
+    
